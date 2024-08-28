84842ae38c12726784cabd3f184e10a236683f0f
Zach Redinger <zach@cloud-team.com>
2024-08-28 21:07:20 +0000

    rename handle to log and fix issues

--- log.go
+++ log.go
@@ -8,30 +8,30 @@ import (
 	"github.com/go-playground/errors/v5"
 )
 
-// Handle returns a http.HandlerFunc that logs any error coming from handlers.
+// Log returns a http.HandlerFunc that logs any error coming from handlers.
 // This provides a more ergonomic feel by allowing errors to be returned from handlers
 //
 // Example usage:
 //
 //	func Handler() http.HandlerFunc {
-//		return httpio.Handle(func(w http.ResponseWriter, r *http.Request) error {
+//		return httpio.Log(func(w http.ResponseWriter, r *http.Request) error {
 //			// do something
 //			return errors.New("error")
 //		})
 //	}
-func Handle(handler func(w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func Log(handler func(w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		if err := handler(w, r); err != nil {
 			cerr := &ClientMessage{}
 			if errors.As(err, &cerr) {
 				messages := strings.Join(Messages(err), "', '")
-				if cerr.msgType < http.StatusInternalServerError {
-					logger.Req(r).Info(cerr)
+				if cerr.msgType < internalServerError {
+					logger.Req(r).Info(err)
 					if messages != "" {
 						logger.Req(r).Infof("['%s']", messages)
 					}
 				} else {
-					logger.Req(r).Error(cerr)
+					logger.Req(r).Error(err)
 					if messages != "" {
 						logger.Req(r).Errorf("['%s']", messages)
 					}
@@ -40,5 +40,5 @@ func Handle(handler func(w http.ResponseWriter, r *http.Request) error) http.Han
 				logger.Req(r).Error(err)
 			}
 		}
-	})
+	}
 }
