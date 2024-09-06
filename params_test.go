c0f573db90d07bbbff67ec36bd6d945cbd62df2b
Joseph Watson <joseph@cloud-team.com>
2024-09-06 17:28:59 +0000

    Add test to params for ccc.UUID

--- params_test.go
+++ params_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/cccteam/ccc"
 	"github.com/go-chi/chi/v5"
 	"github.com/gofrs/uuid"
 )
@@ -449,6 +450,55 @@ func Test_param_ptr_UUID(t *testing.T) {
 			}()
 
 			if gotVal := Param[*uuid.UUID](tt.args.r, tt.args.param); *gotVal != tt.wantVal {
+				t.Errorf("param() = %v, want %v", gotVal, tt.wantVal)
+			}
+		})
+	}
+}
+
+func Test_param_ccc_UUID(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		r     *http.Request
+		param ParamType
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantVal   ccc.UUID
+		wantPanic bool
+	}{
+		{
+			name: "Valid Param",
+			args: args{
+				r:     mockRequest(map[ParamType]string{"fileId": "0020198f-a14e-42ee-b5f8-65a228ba38e7"}),
+				param: ParamType("fileId"),
+			},
+			wantVal: ccc.Must(ccc.UUIDFromString("0020198f-a14e-42ee-b5f8-65a228ba38e7")),
+		},
+		{
+			name: "Invalid Param Panic",
+			args: args{
+				r:     mockRequest(map[ParamType]string{"fileId": "0020198f-a14e-42ee-b5f8-65a228ba38xx"}),
+				param: ParamType("fileId"),
+			},
+			wantPanic: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			defer func() {
+				r := recover()
+				if tt.wantPanic != (r != nil) {
+					t.Errorf("param() panic = %v, wantPanic %v", r, tt.wantPanic)
+				}
+			}()
+
+			if gotVal := Param[ccc.UUID](tt.args.r, tt.args.param); gotVal != tt.wantVal {
 				t.Errorf("param() = %v, want %v", gotVal, tt.wantVal)
 			}
 		})
