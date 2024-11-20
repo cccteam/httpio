42744e7da943896d04b158c55c7335f37ac11078
Joseph Watson <joseph@cloud-team.com>
2024-11-20 18:53:24 +0000

    fix: Fix validation bug where validatorFunc was lost on upgrade to permission checker

--- decode.go
+++ decode.go
@@ -56,6 +56,7 @@ func (d *Decoder[T]) WithPermissionChecker(domainFromReq DomainFromReq, userFrom
 	return &DecoderWithPermissionChecker[T]{
 		userFromReq:   userFromReq,
 		domainFromReq: domainFromReq,
+		validate:      d.validate,
 		enforcer:      enforcer,
 		resourceSet:   rSet,
 		fieldMapper:   d.fieldMapper,
